d7c863e60de39933795919663ee6c159f0501961
Angelo Antonello Borges <aantonelloborges@gmail.com>
2023-07-23 22:17:21 -0300

    não ok

--- modulo_aquecedor_rinnai_brasil/src/rinnai-api.js
+++ modulo_aquecedor_rinnai_brasil/src/rinnai-api.js
@@ -1,68 +1,84 @@
-const axios = require('axios')
-const options = require('./options.js')
-const entities = require('./entities.js')
-const { parseTargetTemperatureToRange, parseRinnaiTemperature, delay, round } = require('./utils.js')
+const axios = require('axios');
+const http = require('http');
+const options = require('./options.js');
+// const entities = require('./entities.js');
+const { parseTargetTemperatureToRange, parseRinnaiTemperature, delay, round } = require('./utils.js');
 const rinnaiApi = axios.create({
-    baseURL: `http://${options.device.host}`
-})
-
+    baseURL: `http://${options.device.host}/`,
+    timeout: 0,
+    headers: {'X-Custom-Header': 'foobar'},
+    httpAgent: new http.Agent({ keepAlive: true })
+});
 
 const setPriority = (requirePriority) => {
-    console.log("[RINNAI API] requirePriority", requirePriority)
-    const priority = requirePriority ? options.haIp : "null"
-    console.log("[RINNAI API] set priority - priority:", priority)
-    return rinnaiApi(`ip:${priority}:pri`)
-        .then(() => {
-            entities.switchPriority.publish('ON')
-            entities.priorityIP.publish(options.haIp)
-            console.log("[RINNAI API] set priority IP to", options.haIp)
-            return true
-        })
-        .catch((e) => {
-            entities.switchPriority.publish('OFF')
-            entities.priorityIP.publish('Não atribuido')
-            console.log("[RINNAI API] set priority error:", e)
-            return false
-        })
+    //console.log("[RINNAI API] requirePriority", requirePriority)
+    const priority = requirePriority ? options.haIp : "null";
+    //console.log("[RINNAI API] set priority - priority:", priority)
+    try {
+        rinnaiApi(`ip:${priority}:pri`)
+            .then((response) => {
+                console.log(e.message);
+                //entities.switchPriority.publish('ON')
+                //entities.priorityIP.publish(options.haIp)
+                //console.log("[RINNAI API] set priority IP to", options.haIp)
+                return true;
+            })
+            .catch((e) => {
+                console.log(e.message);
+                //console.log(e)
+                //console.log("[RINNAI API] set priority error", e?.message || e) //error socket hang up
+                //entities.switchPriority.publish('OFF')
+                //entities.priorityIP.publish('Não atribuido')
+                return false;
+            });
+    }  catch (e) {
+            console.log(e.message);
+            //console.log(e)
+            //console.log("[RINNAI API] set priority error", e?.message || e) //error socket hang up
+            //entities.switchPriority.publish('OFF')
+            //entities.priorityIP.publish('Não atribuido')
+            return false;
+    }
 }
 
-let preventUpdate = false
-const getPreventUpdate = () => preventUpdate
-const startPreventingUpdates = () => preventUpdate = true
-const stopPreventingUpdates = () => preventUpdate = false
+// let preventUpdate = false;
+// const getPreventUpdate = () => preventUpdate;
+// const startPreventingUpdates = () => preventUpdate = true;
+// const stopPreventingUpdates = () => preventUpdate = false;
 
 const setTargetTemperature = async (target, lastTargetTemp = undefined, retries = 0) => {
-    startPreventingUpdates()
+    //startPreventingUpdates();
     try {
-        const targetTemperatureInRange = parseTargetTemperatureToRange(target)
-        let currentTargetTemp = +lastTargetTemp
+        const targetTemperatureInRange = parseTargetTemperatureToRange(target);
+        let currentTargetTemp = +lastTargetTemp;
 
         if (!lastTargetTemp) {
-            const { targetTemperature: stateTargetTemp, priorityIp } = await getState()
-            currentTargetTemp = stateTargetTemp
-            const otherDeviceHasPriority = priorityIp !== "null" && priorityIp !== options.haIp
+            const { targetTemperature: stateTargetTemp, priorityIp } = await getState();
+            currentTargetTemp = stateTargetTemp;
+            console.log("Current Temp:", currentTargetTemp);
+            const otherDeviceHasPriority = priorityIp !== "null" && priorityIp !== options.haIp;
             if (otherDeviceHasPriority) {
-                console.log("[RINNAI API] other device has priority")
-                stopPreventingUpdates()
-                return false
+                console.log("[RINNAI API] other device has priority");
+                //stopPreventingUpdates();
+                return false;
             }
-            await setPriority(true)
+            //setPriority(true)
 
         }
 
-        if (targetTemperatureInRange === currentTargetTemp) {
-            stopPreventingUpdates()
-            //await setPriority(false)
-            return currentTargetTemp
-        }
+        // if (targetTemperatureInRange === currentTargetTemp) {
+        //     stopPreventingUpdates();
+        //     //setPriority(false)
+        //     return currentTargetTemp;
+        // }
 
-        const operation = currentTargetTemp > targetTemperatureInRange ? 'dec' : 'inc'
-        const response = await rinnaiApi(operation)
-        const parsedParams = parseStateParams(response.data)
-        currentTargetTemp = parsedParams.targetTemperature
+        const operation = currentTargetTemp > targetTemperatureInRange ? 'dec' : 'inc';
+        const response = await rinnaiApi(operation);
+        const parsedParams = parseStateParams(response.data);
+        currentTargetTemp = parsedParams.targetTemperature;
 
-        entities.priorityIP.publish(parsedParams.priorityIp)
-        console.log('[RINNAI API] Priority Ip: ',parsedParams.priorityIp)
+        //entities.priorityIP.publish(parsedParams.priorityIp)
+        //console.log('[RINNAI API] Priority Ip: ',parsedParams.priorityIp)
         // const otherDeviceHasPriority = parsedParams.priorityIp !== "null" && parsedParams.priorityIp !== options.haIp
         // if (otherDeviceHasPriority) {
         //     console.log("[RINNAI API] other device has priority")
@@ -71,81 +87,82 @@ const setTargetTemperature = async (target, lastTargetTemp = undefined, retries 
         //     return false
         // }
 
-        if (targetTemperatureInRange === currentTargetTemp) {
-            stopPreventingUpdates()
-            //await setPriority(false)
-            return currentTargetTemp
-        }
+        // if (targetTemperatureInRange === currentTargetTemp) {
+        //     stopPreventingUpdates();
+        //     //setPriority(false)
+        //     return currentTargetTemp;
+        // }
 
-        await delay(60)
+        await delay(60);
 
-        setTargetTemperature(target, currentTargetTemp, 0)
+        setTargetTemperature(target, currentTargetTemp, 0);
     }
     catch (e) {
         if (retries < 5)
-            return setTargetTemperature(target, lastTargetTemp, retries + 1)
-        console.log("[RINNAI API] set temperature error", e?.message || e)
-        stopPreventingUpdates()
-        //await setPriority(false)
-        return false
+            return setTargetTemperature(target, lastTargetTemp, retries + 1);
+        console.log("[RINNAI API] set temperature error", e?.message || e);
+        //stopPreventingUpdates();
+        //setPriority(false)
+        return false;
     }
 }
 
 const setPowerState = async (turnOn) => {
-    const { isPoweredOn } = await getState()
-    if (isPoweredOn === turnOn) return true
-    const response = await rinnaiApi('/lig')
-    return parseStateParams(response.data)
+    const { isPoweredOn } = await getState();;
+    if (isPoweredOn === turnOn) return true;
+    const response = await rinnaiApi('/lig');
+    return parseStateParams(response.data);
 }
 
 
 const pressButton = async (button) => {
-    await setPriority(true)
-    const response = await rinnaiApi(button)
-    const params = parseStateParams(response.data)
-    //await setPriority(false)
-    return params
+    //setPriority(true)
+    const response = await rinnaiApi(button);
+    const params = parseStateParams(response.data);
+    //setPriority(false)
+    return params;
 
 }
 
 
 
 const parseStateParams = (stringifiedParams) => {
-    const params = stringifiedParams.split(',')
-    const targetTemperature = parseRinnaiTemperature(params[7])
-    const isHeating = params[2] === '1'
-    const priorityIp = params[6].split(":")[0]
-    const isPoweredOn = params[0] !== "11"
+    const params = stringifiedParams.split(',');
+    const targetTemperature = parseRinnaiTemperature(params[7]);
+    const isHeating = params[2] === '1';
+    //const priorityIp = params[6].split(":")[0];
+    const isPoweredOn = params[0] !== "11";
 
+    console.log("[RINNAI API] targetTemperature", targetTemperature);
     return {
         targetTemperature,
         isHeating,
-        isPoweredOn,
-        priorityIp
+        isPoweredOn
+        //priorityIp
     }
 }
 
 
 const getState = () => {
-    console.log("[RINNAI API] fetching heater state")
-    return rinnaiApi('/tela_')
-        .then(response => parseStateParams(response.data))
+    console.log("[RINNAI API] fetching heater state");
+    return rinnaiApi('/tela_').then(response => parseStateParams(response.data));
 }
 
 const getDeviceParams = () => {
-    console.log("[RINNAI API] fetching heater parameters")
+    console.log("[RINNAI API] fetching heater parameters");
     return rinnaiApi('/bus')
         .then((response) => {
-            const params = response.data.split(",")
-            const targetTemperature = parseRinnaiTemperature(params[18])
-            const inletTemperature = +params[10] / 100
-            const outletTemperature = +params[11] / 100
-            const currentPowerInKCal = +params[9] / 100
-            const powerInkW = round(currentPowerInKCal * 0.014330754)
-            const isPoweredOn = params[0] !== "11"
+            const params = response.data.split(",");
+            const targetTemperature = parseRinnaiTemperature(params[18]);
+            const inletTemperature = +params[10] / 100;
+            const outletTemperature = +params[11] / 100;
+            const currentPowerInKCal = +params[9] / 100;
+            const powerInkW = round(currentPowerInKCal * 0.014330754);
+            const isPoweredOn = params[0] !== "11";
 
-            const waterFlow = round(+params[12] / 100)
-            const workingTime = +params[4]
+            const waterFlow = round(+params[12] / 100);
+            const workingTime = +params[4];
+            console.log("[RINNAI API] RAW workingTime", workingTime);
             return {
                 targetTemperature,
                 inletTemperature,
@@ -161,21 +178,22 @@ const getDeviceParams = () => {
 const getConsumption = () =>
     rinnaiApi('/consumo')
         .then(response => {
-            const params = response.data.split(',')
-            const [minutes, seconds] = params[0].split(':')
-            const workingTime = (+minutes * 60) + +seconds
-            const water = round(+params[1] / 1000)
-            const gas = round(+params[2] / 9400)
-            return { water, gas, workingTime }
+            const params = response.data.split(',');
+            const [minutes, seconds] = params[0].split(':');
+            const workingTime = (+minutes * 60) + +seconds;
+            const water = round(+params[1] / 1000);
+            const gas = round(+params[2] / 9400);
+            console.log("[RINNAI API] workingTime", workingTime);
+            return { water, gas, workingTime };
         })
 
 
 module.exports = {
     setTargetTemperature,
     getDeviceParams,
-    getPreventUpdate,
+    //getPreventUpdate,
     getState,
-    setPriority,
+    //setPriority,
     setPowerState,
     pressButton,
     getConsumption
@@ -302,4 +320,16 @@ Enfim, vou tentar decompilar o apk do app da Rinnai para veriguar se consigo alg
 Amanhã durante o dia vou configurar com valores corretos e testar em qual campo é retornado o custo do banho.
 
 No entendo ainda precisamos descobrir como realizar o update dessas informações ou obter por get o tempo decorrido do banho.
+*/
+
+
+/*
+/historico
+1:04,6,4,43,1689086642;2:43,6,9,125,1689086539;13:06,6,103,2054,1689081263;1:12,6,3,54,1689077327;17:48,7,235,5117,1689028802;15:39,7,123,2629,1689022175;1:48,7,7,140,-8024;2:01,7,8,134,-8239;5:45,7,22,442,1688976524;11:54,7,94,1939,1688933971;
+[0] - tempo banho (mm:ss)
+[1] - 
+[2] - litros
+[3]
+[4] - timestamp unix epoch
+
 */
